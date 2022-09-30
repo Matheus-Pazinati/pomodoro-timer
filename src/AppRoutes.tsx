8b4e798e650b7d51f3538b2709e03db85512a952
Matheus Pazinati <matheuspazinati@gmail.com>
2022-09-30 05:27:15 -0300

    Cria componente de layout, para reaproveitar o header em todas as paginas

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,13 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
 
+import { DefaultLayout } from './pages/layouts/DefaultLayout'
 import { Home } from './pages/Home'
 import { History } from './pages/History'
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   )
 }
