602ea4d0c8965fe613e2f27ee516ac39b362afcf
Matheus Pazinati <matheuspazinati@gmail.com>
2022-09-30 04:33:54 -0300

    Cria os arquivos das paginas do app, e cria as rotas de cada pagina

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
-import { ThemeProvider } from 'styled-components';
-import { defaultTheme } from './styles/themes/default';
-import { GlobalStyle } from './styles/global';
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
-import { Button } from "./components/Button";
-
+import { BrowserRouter } from 'react-router-dom'
+import { AppRoutes } from './AppRoutes'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
       <GlobalStyle />
-
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="success" />
-      <Button />
     </ThemeProvider>
   )
 }
