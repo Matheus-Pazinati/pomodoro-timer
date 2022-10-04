30323824e48d355a2794a76b54b95a3c9756614c
Matheus Pazinati <matheuspazinati@gmail.com>
2022-10-04 05:00:47 -0300

    Cria estrutura da pagina de Histórico

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,45 @@
+export function History() {
+  return (
+    <div>
+      <h1>Meu histórico</h1>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Exemplo de tarefa</td>
+              <td>30 minutos</td>
+              <td>Há cerca de 20 minutos</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Exemplo de tarefa</td>
+              <td>30 minutos</td>
+              <td>Há cerca de 20 minutos</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Exemplo de tarefa</td>
+              <td>30 minutos</td>
+              <td>Há cerca de 20 minutos</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Exemplo de tarefa</td>
+              <td>30 minutos</td>
+              <td>Há cerca de 20 minutos</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
+}
